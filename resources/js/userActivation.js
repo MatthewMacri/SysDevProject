dbb6e7bd7cec91829a12970fbb0ef670ffad7928
Tarek <2264928@edu.vaniercollege.qc.ca>
2025-04-10 21:35:29 -0400

    added admin change user pass page and small styling changes is userActivationPage

--- resources/js/userActivation.js
+++ resources/js/userActivation.js
@@ -1,37 +1,34 @@
 function showConfirmBox() {
-    const username = document.getElementById("username").value || "unknown";
-    document.getElementById("userID").textContent = username;
-    document.getElementById("confirmBox").style.display = "flex";
-  }
-  
-  function hideConfirmBox() {
-    document.getElementById("confirmBox").style.display = "none";
-  }
-  
+  const username = document.getElementById("username").value || "unknown";
+  document.getElementById("activateUserID").textContent = username;
+  document.getElementById("confirmBox").style.display = "flex";
+}
 
-  function showDeactivateConfirmBox() {
-    const username = document.getElementById("username").value || "unknown";
-    document.getElementById("deactivateUserID").textContent = username;
-    document.getElementById("deactivateConfirmBox").style.display = "flex";
-  }
-  
-  function hideDeactivateConfirmBox() {
-    document.getElementById("deactivateConfirmBox").style.display = "none";
-  } 
+function hideConfirmBox() {
+  document.getElementById("confirmBox").style.display = "none";
+}
 
-  document.addEventListener("DOMContentLoaded", () => {
-    const activateButton = document.querySelector(".activateButton");
-    activateButton.addEventListener("click", function (e) {
-      e.preventDefault();
-      showConfirmBox();
-    }); 
+function showDeactivateConfirmBox() {
+  const username = document.getElementById("username").value || "unknown";
+  document.getElementById("deactivateUserID").textContent = username;
+  document.getElementById("deactivateConfirmBox").style.display = "flex";
+}
+
+function hideDeactivateConfirmBox() {
+  document.getElementById("deactivateConfirmBox").style.display = "none";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const activateButton = document.querySelector(".activateButton");
+  const deactivateButton = document.querySelector(".deactivateButton");
+
+  activateButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    showConfirmBox();
   });
 
-  document.addEventListener("DOMContentLoaded", () => {
-    const deactivateButton = document.querySelector(".deactivateButton");
-    deactivateButton.addEventListener("click", function (e) {
-      e.preventDefault();
-      showConfirmBox();
-    });
+  deactivateButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    showDeactivateConfirmBox();
   });
-  
+});
